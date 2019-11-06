7f1865d6c22326159d55b1347eb28c16fd0ef4f7
Simon Ser <contact@emersion.fr>
2019-11-06 11:18:56 +0100

    Remove wlroots texture hack
    Use the new wlr_gles2_texture APIs to access the texture GL attributes.

--- include/render.h
+++ include/render.h
@@ -6,7 +6,6 @@
 #include <EGL/eglext.h>
 #include <GLES2/gl2.h>
 #include <openxr/openxr.h>
-#include <wlr/render/wlr_texture.h>
 
 struct wxrc_xr_view;
 struct wxrc_server;
@@ -14,28 +13,6 @@ struct wxrc_server;
 struct wxrc_gl {
 	GLuint grid_program;
 	GLuint texture_program;
-};
-
-/* TODO: remove this private wlroots definition */
-
-struct wlr_gles2_texture {
-	struct wlr_texture wlr_texture;
-	struct wlr_egl *egl;
-
-	// Basically:
-	//   GL_TEXTURE_2D == mutable
-	//   GL_TEXTURE_EXTERNAL_OES == immutable
-	GLenum target;
-	GLuint tex;
-
-	EGLImageKHR image;
-
-	int width, height;
-	bool inverted_y;
-	bool has_alpha;
-
-	// Only affects target == GL_TEXTURE_2D
-	enum wl_shm_format wl_format; // used to interpret upload data
 };
 
 bool wxrc_gl_init(struct wxrc_gl *gl);
